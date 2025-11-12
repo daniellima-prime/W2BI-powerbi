43d6b907e8deea62227ccb0cd6e25f4a0d199f42
Daniel Lopes de Lima <danimila@gmail.com>
2025-11-12 09:45:04 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  return (
+    <iframe
+      title="Power BI Report"
+      src="https://app.powerbi.com/view?r=eyJrIjoiNTE0MDkzYWQtNGQ2OC00Zjg3LTkxODktN2U1NDQzMzkwZTZhIiwidCI6ImJmNDQ1ODhhLTk2NDQtNGZmMC05ZjRiLTU4ZGMwMzdjZDJmNiJ9"
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+      }}
+      frameBorder="0"
+      allowFullScreen
+    ></iframe>
+  )
+}
+
+export default App
