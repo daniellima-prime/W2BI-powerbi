73c02e5597ca39e18bdad9395da00ab1b59083ca
daniellima-prime <daniel.lima@consultoriaprime.com>
2025-11-13 16:57:29 -0300

    Adicionar bolaanimada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css"; // Certifique-se que App.css contém a classe .circulo
+import "../App.css";
 
 function Home() {
   const navigate = useNavigate();
@@ -9,19 +9,19 @@ function Home() {
   const [erro, setErro] = useState("");
 
   const handleAcessar = () => {
-    const emailCorreto = "teste@consultoriaprime.com"; // e-mail fixo para teste
-    const senhaCorreta = "12345";               // senha fixa para teste
+    const emailCorreto = "teste@consultoriaprime.com";
+    const senhaCorreta = "12345";
 
     if (email === emailCorreto && senha === senhaCorreta) {
-      navigate("/report"); // redireciona se email e senha estiverem corretos
+      navigate("/report");
     } else {
       setErro("E-mail ou senha incorretos. Tente novamente.");
     }
   };
 
   return (
+    
     <div className="home-container">
-      <div className="fundo"></div>
       <div className="circulo"></div>
 
       <h1 className="home-title">Bem-vindo a Works2BI</h1>
